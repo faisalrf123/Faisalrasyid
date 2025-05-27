a1c35c9c85862f7089c02c1b9894f3e592ecbde7
faisalrf123 <faisalariefalrasyid@gmail.com>
2025-05-27 07:53:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,12 @@ st.write(
 )
 
 st.image("17477052906911695653576004249561.jpg", width=200) 
+
+st.title("Sman 20 Bandung") 
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
+angka = st.number_input("tulis sebuah angka:", value=0, step=1) 
+
+if (angka % 2) ==0:
+ st.write(f"{angka} adalah Bilangan Genap") 
+else:
+ st.write(f"{angka}adalah Bilangan Ganjil") 
